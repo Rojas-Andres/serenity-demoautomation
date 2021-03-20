0f40359e394eb40a78ea3563765fbcaa46a3042b
RojasxAndres <afrojaslondono@gmail.com>
2021-03-19 22:04:04 -0500

    Inicializando el proyecto

---
+++ build/resources/test/features/demoAutomation.feature
@@ -0,0 +1,7 @@
+@stories
+Feature: Demo Automation
+  AS USWE
+  Scenario: Registration in demo automation
+    Given  andres wants to register in demo automation
+    When he fills out the form
+    Then he finds
---
+++ src/test/java/proyecto7/stepdefinitions/DemoStepDefinitions.java
@@ -0,0 +1,34 @@
+package proyecto7.stepdefinitions;
+
+import cucumber.api.java.Before;
+
+import java.util.List;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+public class DemoStepDefinitions {
+
+    @Given("^andres wants to register in demo automation$")
+    public void andresWantsToRegisterInDemoAutomation() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new PendingException();
+    }
+
+
+    @When("^he fills out the form$")
+    public void heFillsOutTheForm() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he finds$")
+    public void heFinds() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
